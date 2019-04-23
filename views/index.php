013c07bcae33a59d07bdd5540a5b1db389e87786
walidtt17 <walid.tayeche@esprit.tn>
2019-04-23 22:27:36 +0100

    Update index.php

--- views/index.php
+++ views/index.php
@@ -1,150 +1,7 @@
 
 <?php
-session_start();
-//index.php
-$connect = mysqli_connect("localhost", "root", "", "site_web");
-function make_query($connect)
-{
- $query = "SELECT * FROM pub where pos =1";
- $result = mysqli_query($connect, $query);
- return $result;
-}
+include 'pub_front.php';
 
-function make_query2($connect)
-{
- $query = "SELECT * FROM pub where pos =2";
- $result = mysqli_query($connect, $query);
- return $result;
-}
-function make_query3($connect)
-{
- $query = "SELECT * FROM pub where pos =3";
- $result = mysqli_query($connect, $query);
- return $result;
-}
-function make_slide_indicators($connect)
-{
- $output = ''; 
- $count = 0;
- $result = make_query($connect);
- while($row = mysqli_fetch_array($result))
- {
-  if($count == 0)
-  {
-   $output .= '         
-   <li data-target="#dynamic_slide_show" data-slide-to="'.$count.'" class="active"></li>
- 
-
-   ';
-  }
-  else
-  {
-   $output .= '
-   <li data-target="#dynamic_slide_show" data-slide-to="'.$count.'"></li>
-   ';
-  }
-  $count = $count + 1;
- }
- return $output;
-}
-
-function make_slides($connect)
-{
- $output = '';
- $count = 0;
- $result = make_query($connect);
- while($row = mysqli_fetch_array($result))
- {
-  if($count == 0)
-  {
-   $output .= '<div class="item active">';
-  }
-  else
-  {
-   $output .= '<div class="item">';
-  }
-  $output .= '
-   <img id ="skip253" src="back/crud_pub/'.$row["im"].'" alt="'.$row["pos"].'" />
-
-   <div class="carousel-caption">
-
-   </div>
-<form method="POST" action="nblike.php">
-  <button class="btn btn-primary btn-sm" id="butt" type="submit" name="done"> Like </button>
-  <button class="btn btn-primary btn-sm" id="butt1" type="submit" name="idone"> Dislike </button>
-      <input type="hidden" value="'.$row["id"].'" name ="id" >
-</form>
-  </div>
-  ';
-  $count = $count + 1;
- }
- return $output;
-}
-function make_slides2($connect)
-{
- $output = '';
- $count = 0;
- $result = make_query2($connect);
- while($row = mysqli_fetch_array($result))
- {
-  if($count == 0)
-  {
-   $output .= '<div class="item active">';
-  }
-  else
-  {
-   $output .= '<div class="item">';
-  }
-  $output .= '
-   <img id ="skip253" src="back/crud_pub/'.$row["im"].'" alt="'.$row["pos"].'" />
-
-   <div class="carousel-caption">
-
-   </div>
-<form method="POST" action="nblike.php">
-  <button class="btn btn-primary btn-sm" id="butt" type="submit" name="done"> Like </button>
-  <button class="btn btn-primary btn-sm" id="butt1" type="submit" name="idone"> Dislike </button>
-      <input type="hidden" value="'.$row["id"].'" name ="id" >
-</form>
-  </div>
-  ';
-  $count = $count + 1;
- }
- return $output;
-}
-
-function make_slides3($connect)
-{
- $output = '';
- $count = 0;
- $result = make_query3($connect);
- while($row = mysqli_fetch_array($result))
- {
-  if($count == 0)
-  {
-   $output .= '<div class="item active">';
-  }
-  else
-  {
-   $output .= '<div class="item">';
-  }
-  $output .= '
-   <img id ="skip253" src="back/crud_pub/'.$row["im"].'" alt="'.$row["pos"].'" />
-
-   <div class="carousel-caption">
-
-   </div>
-<form method="POST" action="nblike.php">
-  <button class="btn btn-primary btn-sm" id="butt" type="submit" name="done"> Like </button>
-  <button class="btn btn-primary btn-sm" id="butt1" type="submit" name="idone"> Dislike </button>
-      <input type="hidden" value="'.$row["id"].'" name ="id" >
-</form>
-  </div>
-  ';
-  $count = $count + 1;
- }
- return $output;
-}
 ?>
 
 
@@ -174,7 +31,7 @@ position: absolute;
   #pop2{
           height: 200px;
      width: 900px;
-        bottom: -275%;
+        bottom: -260%;
      right: 18%;
      position: absolute;
   }
@@ -257,6 +114,7 @@ right: 18%;
    <?PHP
   include'header.php' ;
 ?>
+
     <!-- END nav -->
   
   
