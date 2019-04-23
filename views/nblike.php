855314f464bc894e0cfd047f29068035bc83ee8c
walidtt17 <walid.tayeche@esprit.tn>
2019-04-23 22:27:39 +0100

    Update nblike.php

--- views/nblike.php
+++ views/nblike.php
@@ -1,9 +1,10 @@
 <?php 
 $id=$_POST['id'];
+$connect = mysqli_connect("localhost", "root", "", "site_web");
 if (isset($_POST['done'])) {
 	# code...
 
-$connect = mysqli_connect("localhost", "root", "", "site_web");
+
  $query = "UPDATE pub SET nb=nb+1 WHERE id='$id'";
  $result = mysqli_query($connect, $query);
  header("Location: index.php");
@@ -11,7 +12,7 @@ $connect = mysqli_connect("localhost", "root", "", "site_web");
 if (isset($_POST['idone'])) {
 	# code...
 
-$connect = mysqli_connect("localhost", "root", "", "site_web");
+
  $query = "UPDATE pub SET nb_dis=nb_dis+1 WHERE id='$id'";
  $result = mysqli_query($connect, $query);
  header("Location: index.php");
@@ -25,7 +26,7 @@ $connect = mysqli_connect("localhost", "root", "", "site_web");
 if (isset($_POST['done1'])) {
 	# code...
 
-$connect = mysqli_connect("localhost", "root", "", "site_web");
+
  $query = "UPDATE pub SET nb=nb+1 WHERE pos='2'";
  $result = mysqli_query($connect, $query);
  header("Location: index.php");
@@ -33,7 +34,7 @@ $connect = mysqli_connect("localhost", "root", "", "site_web");
 if (isset($_POST['done2'])) {
 	# code...
 
-$connect = mysqli_connect("localhost", "root", "", "site_web");
+
  $query = "UPDATE pub SET nb=nb+1 WHERE pos='3'";
  $result = mysqli_query($connect, $query);
  header("Location: index.php");
