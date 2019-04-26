452c8cc2bc90560c8e1168164ac067f50ec5c7ef
belkisbaccar <belkis.baccar@esprit.tn>
2019-04-26 23:50:32 +0100

    Update supprimerFacture.php

--- views/back/supprimerFacture.php
+++ views/back/supprimerFacture.php
@@ -1,7 +1,6 @@
 <?PHP
-include "C:/wamp/www/crud/config.php";
-include "../../../core/factureC.php";
-
+include "C:/wamp64/www/website_GYM/config.php";
+include "C:/wamp64/www/website_GYM/core/factureC.php";
 $factureC=new factureC();
 if (isset($_POST["id"])){
 	$factureC->supprimerFactureID($_POST["id"]);
