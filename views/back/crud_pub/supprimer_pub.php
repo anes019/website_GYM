8a253574f93a65d2f87d253321647e0a4fc52331
walidtt17 <walid.tayeche@esprit.tn>
2019-04-26 04:13:42 +0100

    Update supprimer_pub.php

--- views/back/crud_pub/supprimer_pub.php
+++ views/back/crud_pub/supprimer_pub.php
@@ -1,9 +1,12 @@
 <?PHP
+
 include "C:/wamp64/www/website_GYM/core/pubC.php";
 $pubC=new pubC();
-if (isset($_POST["id"])){
-	$pubC->supprimerPub($_POST["id"]);
-	header('Location: ../afficher_data.php');
-}
+if (isset($_GET["id"])){
+	$pubC->supprimerPub($_GET["id"]);
 
+echo"<script language=\"javascript\">";
+echo"window.location.href='../afficher_data.php?page=1&sort=2'";
+echo"</script>";
+}
 ?>
