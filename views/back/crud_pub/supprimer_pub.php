3c58338f2a8f915f003dad75aebb90e3affbc9d2
walidtt17 <walid.tayeche@esprit.tn>
2019-04-26 16:44:48 +0100

    Update supprimer_pub.php

--- views/back/crud_pub/supprimer_pub.php
+++ views/back/crud_pub/supprimer_pub.php
@@ -6,7 +6,7 @@ if (isset($_GET["id"])){
 	$pubC->supprimerPub($_GET["id"]);
 
 echo"<script language=\"javascript\">";
-echo"window.location.href='../afficher_data.php?page=1&sort=2'";
+echo"window.location.href='../afficher_data.php?page=1&sort=2&trie=0'";
 echo"</script>";
 }
 ?>
