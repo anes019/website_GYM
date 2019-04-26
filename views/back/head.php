beabec191b9446b0ea437c30dd80a5a5bf2525ac
walidtt17 <walid.tayeche@esprit.tn>
2019-04-26 16:44:51 +0100

    Update head.php

--- views/back/head.php
+++ views/back/head.php
@@ -27,7 +27,7 @@
                                 <i class="fas fa-table"></i>Gestion des Abonements</a>
                         </li>   
                         <li >
-                            <a href="afficher_data.php?page=<?PHP echo 1; ?>&sort=2">
+                            <a href="afficher_data.php?page=<?PHP echo 1; ?>&sort=2&trie=<?PHP echo 0 ?>">
                                 <i class="fas fa-table"></i>Gestion des Pubs </a>
                         </li>
                         <li >
