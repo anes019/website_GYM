9f77fbee02cbdf3ab796c3209f0f412478338649
belkisbaccar <belkis.baccar@esprit.tn>
2019-04-27 20:09:23 +0100

    Update mail_php.php

--- views/mail_php.php
+++ views/mail_php.php
@@ -9,7 +9,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'abdelhamid.jazi@esprit.tn';                 // SMTP username
-$mail->Password = 'maindoucha12';                           // SMTP password
+$mail->Password = '';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
