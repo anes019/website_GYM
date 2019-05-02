30534a1042924b2f77b29b15b32e246e08857320
waliddtt17 <walid.tayeche@esprit.tn>
2019-05-02 16:31:00 +0100

    Update program.php

--- views/program.php
+++ views/program.php
@@ -10,7 +10,115 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
      <script src="validerchampcour.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed:100,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed:100,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/cart.css"/>
+
+
+
+    <link href="css/font-face.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+<style>
+
+
+
+
+*{margin:0px; padding:0px; font-family:Helvetica, Arial, sans-serif;}
+
+/* Full-width input fields */
+input[type=text], input[type=password] {
+    width: 90%;
+    padding: 12px 20px;
+    margin: 8px 26px;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+  font-size:16px;
+}
+
+/* Set a style for all buttons */
+
+button:hover {
+    opacity: 0.8;
+}
+
+/* Center the image and position the close button */
+.imgcontainer {
+    text-align: center;
+    margin: 24px 0 12px 0;
+    position: relative;
+}
+.avatar {
+    width: 200px;
+  height:200px;
+    border-radius: 50%;
+}
+
+/* The Modal (background) */
+.modal {
+  display:none;
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgba(0,0,0,0.4);
+}
+
+/* Modal Content Box */
+.modal-content {
+    background-color: #fefefe;
+    margin: 4% auto 15% auto;
+    border: 1px solid #888;
+    width: 40%; 
+  padding-bottom: 30px;
+}
+
+/* The Close Button (x) */
+.close {
+    position: absolute;
+    right: 25px;
+    top: 0;
+    color: #000;
+    font-size: 35px;
+    font-weight: bold;
+}
+.close:hover,.close:focus {
+    color: red;
+    cursor: pointer;
+}
+
+/* Add Zoom Animation */
+.animate {
+    animation: zoom 0.6s
+}
+@keyframes zoom {
+    from {transform: scale(0)} 
+    to {transform: scale(1)}
+}
+</style>
     <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
     <link rel="stylesheet" href="css/animate.css">
     
@@ -157,10 +265,22 @@ session_start();
             <form action="ajouterCour.php" method="POST" enctype="multipart/form-data" class="appointment-form">
               <div class="d-md-flex">
                 <div class="form-group">
-                  <input  type="text" name="nom" id="nom" class="form-control" placeholder="Nom">
+                  <input  type="text" name="nom" id="nom" class="form-control" placeholder="<?php 
+                   if(empty($_SESSION['l']))
+                   echo "Nom";
+                  else
+                  echo $_SESSION['l']; 
+
+                  ?>" disabled >
                 </div>
                 <div class="form-group ml-md-4">
-                  <input type="text" name="prenom" id="prenom" class="form-control" placeholder="Prenom">
+                  <input type="text" name="prenom" id="prenom" class="form-control" placeholder="<?php 
+                   if(empty($_SESSION['l']))
+                   echo "Prenom";
+                  else
+                  echo $_SESSION['p']; 
+
+                  ?>" disabled>
                 </div>
               </div>
               <div class="d-md-flex">
@@ -210,7 +330,18 @@ session_start();
                   <textarea name="message" id="mess" cols="30" rows="2" class="form-control" id="message" placeholder="Message"></textarea>
                 </div>
                 <div class="form-group ml-md-4">
-                  <input onclick="validerchamp()" type="submit" value="reserver" class="btn btn-primary py-3 px-4">
+            	<?php
+            	if(empty( $_SESSION['l']))
+            		{?>
+            		
+            		<a onclick="document.getElementById('modal-wrapper').style.display='block'" class="btn btn-primary py-3 px-4">reserver</a>
+                 <?php
+             }
+                  else { ?>
+                  	<input onclick="validerchamp()" type="submit" value="reserver" class="btn btn-primary py-3 px-4">
+                       <?php
+             }
+?>
                 </div>
               </div>
             </form>
@@ -218,6 +349,36 @@ session_start();
         </div>
       </div>
     </section>
+ 
+  <div id="modal-wrapper" class="modal">
+  
+  <form class="modal-content animate"  method="POST" action="../core/check_acc4.php">
+        
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('modal-wrapper').style.display='none'" class="close" title="Close PopUp">&times;</span>
+
+      <h1 style="text-align:center">LOGIN</h1>
+    </div>
+
+    <div class="container">
+      <input type="text" name="email" id="mail" placeholder="Email address or user name" >
+      <input type="password" name="pass" id="pass" placeholder="Enter Password" >        
+      <button type="submit" style="background-color: #ff3399;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 26px;
+    border: none;
+    cursor: pointer;
+    width: 90%;
+  font-size:20px;">Login</button>
+        
+      <a href="MDP oublie.php" style="text-decoration:none; float:right; margin-right:34px; margin-top:26px;">Forgot Password ?</a>
+    </div>
+    
+  </form>
+  
+</div>
+
 
 
     <footer class="ftco-footer ftco-section img">
@@ -320,6 +481,15 @@ session_start();
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
   <script src="js/google-map.js"></script>
   <script src="js/main.js"></script>
-    
+   <script>
+// If user clicks anywhere outside of the modal, Modal will close
+
+var modal = document.getElementById('modal-wrapper');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script> 
   </body>
 </html>
