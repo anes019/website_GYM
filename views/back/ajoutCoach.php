ab717cf184d7d0fee8f29f7119f4b38d7392febf
waliddtt17 <walid.tayeche@esprit.tn>
2019-05-15 12:22:00 +0100

    walid

--- views/back/ajoutCoach.php
+++ views/back/ajoutCoach.php
@@ -34,62 +34,15 @@
     <link href="css/theme.css" rel="stylesheet" media="all">
 
 </head>
+<?php
+include "head.php"
+?>
 
-<body class="animsition">
-    <div class="page-wrapper">
-        <!-- HEADER MOBILE-->
-        <header class="header-mobile d-block d-lg-none">
-            <div class="header-mobile__bar">
-                <div class="container-fluid">
-                    <div class="header-mobile-inner">
-                        <a class="logo" href="index.html">
-                            <img src="images/icon/logo.png"  />
-                        </a>
-                        <button class="hamburger hamburger--slider" type="button">
-                            <span class="hamburger-box">
-                                <span class="hamburger-inner"></span>
-                            </span>
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <nav class="navbar-mobile">
-                <div class="container-fluid">
-                    <ul class="navbar-mobile__list list-unstyled">                                                
-                        <li>
-                            <a href="form.html">
-                                <i class="far fa-check-square"></i>Coaches</a>
-                        </li>                      
-                    </ul>
-                </div>
-            </nav>
-        </header>
-        <!-- END HEADER MOBILE-->
-
-
-        <!-- MENU SIDEBAR-->
-        <aside class="menu-sidebar d-none d-lg-block">
-            <div class="logo">
-                <a href="#">
-                    <img src="images/icon/logo.png"  />
-                </a>
-            </div>
-            <div class="menu-sidebar__content js-scrollbar1">
-                <nav class="navbar-sidebar">
-                    <ul class="list-unstyled navbar__list">
-                        <li class="active">
-                            <a href="ajoutCoach.php">
-                                <i class="far fa-check-square"></i>Coaches</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </aside>
         <!-- END MENU SIDEBAR-->
 
 
         <!-- PAGE CONTAINER-->
-        <div class="page-container">
+        
             <!-- HEADER DESKTOP-->
             <header class="header-desktop">
                 <div class="section__content section__content--p30">
--- views/back/crud_promo/ajouter_promo.php
+++ views/back/crud_promo/ajouter_promo.php
@@ -43,42 +43,131 @@
     <link href="css/theme.css" rel="stylesheet" media="all">
 </head>
 <body class="animsition">
-      <div class="page-wrapper">
-        
-
-
-        <!-- MENU SIDEBAR-->
-         <div class="page-wrapper">
+    <div class="page-wrapper">
 
         <aside class="menu-sidebar d-none d-lg-block">
-            
-            <div class="menu-sidebar__content js-scrollbar1">
-                <nav class="navbar-sidebar">
-                    <ul class="list-unstyled navbar__list">
+            <div style="width: 285.4px; height: 150px;" class="logo">
+                <a href="../dash.php">
+                    <img style="position: absolute; top: 0px; right:70px; height: 150px; "   src="../images/icon/1.png" alt="BF Academie" />
+                </a>
+            </div>
+            <div  class="menu-sidebar__content js-scrollbar1">
+                <nav  class="navbar-sidebar">
+                    <ul  class="list-unstyled navbar__list">
 
                         <li >
-                            <a class="js-arrow" href="../crud_pub/ajouter_pub.php">
-                                <i class="fas fa-table"></i>formulaire pub</a>                                      
+                            <a href="../dash.php">
+                                <i class="fas fa-table"></i>Dashboard</a>                                      
+                        </li>
+                                <ul class="navbar-mobile__list list-unstyled">
+                        <li class="has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>service commercial</a>
+                            <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+                             <li >
+                            <a href="../affichage_categorie.php">
+                                <i class="fas fa-table"></i>Gestion des Categories</a>
                         </li>
                         <li >
-                            <a href="ajouter_promo.php">
-                                <i class="fas fa-table"></i>formulaire promo</a>
+                            <a href="../affichage_produits.php">
+                                <i class="fas fa-table"></i>Gestion des Produits</a>
                         </li>
-                       
+                        <li >
+                            <a href="../affichage_abonement.php">
+                                <i class="fas fa-table"></i>Gestion des Abonements</a>
+                        </li>   
+                            </ul>
+                        </li> 
                    </ul>
+                        <ul class="navbar-mobile__list list-unstyled">
+                        <li class="has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>Pub et Promo</a>
+                            <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+                               
+                                <li >
+                            <a href="../afficher_data.php?page=<?PHP echo 1; ?>&sort=2&trie=<?PHP echo 0 ?>">
+                                <i class="fas fa-table"></i>Gestion des Pubs </a>
+                        </li>
+                        <li >
+                            <a href="../afficher_data1.php">
+                                <i class="fas fa-table"></i>Gestion des Promos</a>
+                        </li> 
+                                
+                            </ul>
+                        </li> 
+                   </ul>
+                      
+                       
+                               <ul class="navbar-mobile__list list-unstyled">
+                        <li class="has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>Gestion de la salle</a>
+                            <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+                                <li>
+                                    <a href="../affichercoach.php"><i class="fas fa-table"></i>afficher les Coaches</a>
+                                </li>
+                                <li>
+                                    <a href="../ajoutCoach.php"><i class="fas fa-table"></i>ajouter Coache</a>
+                                </li>
+                                <li >
+                            <a href="../affichercour.php">
+                                <i class="fas fa-table"></i>Gestion des seances</a>
+                         </li>
+                            </ul>
+                        </li> 
+                   </ul>
+                   <ul class="navbar-mobile__list list-unstyled">
+                        <li class="has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>service client</a>
+                            <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+                                <li >
+                            <a href="../table_Client.php">
+                                <i class="fas fa-table"></i>Gestion des Clients</a>
+                         </li>
+                         <li>     
+                                <a href="../table_Reclamtion.php">
+                                <i class="fas fa-table"></i>Gestion des Reclamations</a>
+                        </li>  
+                      
+                                
+                            </ul>
+                        </li> 
+                   </ul>
+                       <ul class="navbar-mobile__list list-unstyled">
+                        <li class="has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>service commande</a>
+                            <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+                         <li>     
+                                <a href="../afficherFacture.php">
+                                <i class="fas fa-table"></i>Gestion des Factures</a>
+                        </li> 
+                        <li>     
+                            <a href="../afficherCommande.php?page=<?PHP echo ""; ?>&sort=2&trie=<?PHP echo 0 ?>">
+                                <i class="fas fa-table"></i>Gestion des Commandes </a>
+                             </li>  
+                      
+                                
+                            </ul>
+                        </li> 
+                   </ul>
+                         
+                                
+                            
+                        
                    
                 </nav>
             </div>
         </aside>
-        <!-- END MENU SIDEBAR-->
 
-
-        <!-- PAGE CONTAINER-->
-        <div class="page-container">
-
+             <div class="page-container">
             <!-- HEADER DESKTOP-->
-            
-            <!-- HEADER DESKTOP-->
+               
+        
+
+
            <br>
      
             <div class="col-lg-2 m-auto">
@@ -112,7 +201,7 @@
  </div> 
 <br>
  <div class="col-12 col-md-9">
-      <label> ID_Produit_Promo: </label>
+      <label> Nom Du Produit: </label>
  <?php
 
 // php select option value from database
@@ -127,7 +216,7 @@ $databaseName = "site_web";
 $connect = mysqli_connect($hostname, $username, $password, $databaseName);
 
 // mysql select query
-$query = "SELECT id FROM produits where etat ='false'";
+$query = "SELECT nom FROM produits where etat ='false'";
 
 // for method 1
 
